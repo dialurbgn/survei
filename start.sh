e8c060fd7288cf94a51af1b4271f8b016be91e0d
Hanafi Ginting LAPTOP <hanafi14@gmail.com>
2025-12-19 01:42:10 +0700

    Initial commit

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-# Start rsyslog dan fail2ban
-service rsyslog start
-service fail2ban start
+# Hapus service yang tidak ada
+# service rsyslog start
+# service fail2ban start
 
 # Jalankan PM2 untuk app Node.js (jika ada process.yml)
 if [ -f /var/www/html/process.yml ]; then
   pm2-runtime /var/www/html/process.yml
 fi
 
-# Jalankan PHP-FPM sebagai PID 1 di foreground
+# Jalankan PHP-FPM di foreground
 exec php-fpm -F
