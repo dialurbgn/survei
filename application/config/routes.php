3aa494bff37232f0f8590b436bb73fa39a766e6f
Hanafi Ginting LAPTOP <hanafi14@gmail.com>
2025-12-19 10:55:05 +0700

    Initial commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'frontend';
+$route['default_controller'] = 'frontend/survei_pm';
 $route['404_override'] = 'notfound_404';
 $route['translate_uri_dashes'] = FALSE;
 $route['submit'] = 'login/actiondata';
