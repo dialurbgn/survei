2fc22c0c897fe4cefb443ff326633d694bfb052f
Hanafi Ginting LAPTOP <hanafi14@gmail.com>
2026-02-02 10:03:56 +0700

    update role

--- application/modules/dashboard/controllers/Dashboard.php
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -40,7 +40,8 @@ class Dashboard extends MX_Controller {
 			$this->ortyd->session_check();
 			$this->ortyd->access_check($this->module);
 			
-			$gid = $this->session->userdata('group_id');
+			$userid = $this->session->userdata('userid');
+			$gid = $this->ortyd->select2_getname($userid,'users_data','id','gid');
 			if($gid == 3){
 				redirect('survei', 'refresh');			
 			}
